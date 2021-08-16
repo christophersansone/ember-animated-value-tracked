e9b1417c66209e1b8a42102ff95a955acf245ad1
christophersansone <chris@voltage.io>
2021-08-16 15:28:02 -0400

    root url

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'animated',
     environment,
-    rootURL: '/',
+    rootURL: '/dist/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
